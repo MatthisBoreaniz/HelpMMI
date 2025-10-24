e67195046e3a192c5d6c4361048d9bf2acce83b3
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2025-10-24 16:18:26 +0200

    menu desktop

---
+++ src/components/Menu.vue
@@ -0,0 +1,47 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import useAuth from '@/composables/useAuth';
+const { currentUser } = useAuth();
+</script>
+
+
+
+<template>
+  <nav class="bg-white p-4 md:w-1/5">
+    <div class="bg-Rose p-4 rounded-md">
+        <ul class="flex flex-col space-y-4 text-lg">
+          <li>
+            <RouterLink to="/" class="text-black font-agrandir hover:text-Blanc font-bold flex items-center gap-2">
+              <img src="@/assets/Icones/maison.png" alt="Accueil" class="w-6 h-6 mr-2" />
+              Accueil
+            </RouterLink>
+          </li>
+          <li v-if="currentUser">
+            <RouterLink to="/about" class="text-black font-agrandir hover:text-Blanc font-bold flex items-center gap-2">
+              <img src="@/assets/Icones/aides.png" alt="Mes Aides" class="w-5h-5 mr-2" />
+              Mes Aides
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/contact" class="text-black font-agrandir hover:text-Blanc font-bold flex items-center gap-2">
+              <img src="@/assets/Icones/simulation.png" alt="Simulation" class="w-5h-5 mr-2" />
+              Simulation
+            </RouterLink>
+          </li>
+          <li v-if="currentUser">
+            <RouterLink to="/contact" class="text-black font-agrandir hover:text-Blanc font-bold flex items-center gap-2">
+              <img src="@/assets/Icones/infos.png" alt="Mes Informations" class="w-5h-5 mr-2" />
+              Mes Informations
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/contact" class="text-black font-agrandir hover:text-Blanc font-bold flex items-center gap-2">
+              <img src="@/assets/Icones/annuaire.png" alt="Annuaires" class="w-5h-5 mr-2" />
+              Annuaires
+            </RouterLink>
+          </li>
+        </ul>
+    </div>
+  </nav>
+
+</template>
