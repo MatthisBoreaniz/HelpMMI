8bf9358fe96360c4d6d123d6c8077ac6f176d8f7
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2025-10-27 22:15:30 +0100

    pour telephone

--- src/components/LogInOut.vue
+++ src/components/LogInOut.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
-import { LogInWithGoogle } from '@/backend';
-import useAuth from '@/composables/useAuth';
-import { pb } from '@/backend';
-import ImgPb from '@/pages/ImgPb.vue';
-const { currentUser } = useAuth();
+import { LogInWithGoogle } from '@/backend'
+import useAuth from '@/composables/useAuth'
+import { pb } from '@/backend'
+import ImgPb from '@/pages/ImgPb.vue'
+const { currentUser } = useAuth()
 
-const GoogleImage = await pb.collection('LogosAndImages').getFirstListItem('nom="GoogleLogo"');
+const GoogleImage = await pb.collection('LogosAndImages').getFirstListItem('nom="GoogleLogo"')
 </script>
-
 
 <template>
   <div>
     <div v-if="!currentUser">
-      <button @click="LogInWithGoogle" class="p-2 mb-5 bg-white text-Bleu border border-Rose rounded-md flex items-center gap-2 hover:bg-blue-100 text-sm">
+      <button
+        @click="LogInWithGoogle"
+        @touchstart.prevent="LogInWithGoogle"
+        class="p-2 mb-5 bg-white text-Bleu border border-Rose rounded-md flex items-center gap-2 hover:bg-blue-100 text-sm"
+      >
         <ImgPb
-            v-if="GoogleImage"
-            :record="GoogleImage"
-            :filename="GoogleImage.image"
-            class="w-3 h-auto"
-          />  
+          v-if="GoogleImage"
+          :record="GoogleImage"
+          :filename="GoogleImage.image"
+          class="w-3 h-auto"
+        />
         Se connecter avec google
       </button>
-    </div> 
+    </div>
   </div>
 </template>
