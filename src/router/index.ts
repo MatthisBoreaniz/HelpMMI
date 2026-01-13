e491da767f53ae851f7200218c022eba4dc59d9e
Matthis Boreaniz <matthisonepiece@gmail.com>
2026-01-13 13:30:43 +0100

    fix probleme scroll

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,10 @@ import { routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes, 
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition
+    return { top: 0 }
+  }
 })
 
 
